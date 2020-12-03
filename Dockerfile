52ce6159c7689e1d9ccf8ad46e6047c7e3e544fe
lucasorsi <lorsi@itba.edu.ar>
2020-12-02 21:55:28 -0300

    Development environment success

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM osrf/ros:melodic-desktop-full
+RUN echo "source "/opt/ros/$ROS_DISTRO/setup.bash >> ~/.bashrc
+RUN mkdir -p ~/catkin_ws/src
+CMD ['bin/bash']
--- development.sh
+++ development.sh
@@ -1,8 +1,8 @@
 if [ "$(docker ps -q -f name=ros-dev)" ]; then
-    docker exec -it ros-dev bash -c 'echo "source "/opt/ros/$ROS_DISTRO/setup.bash >> ~/.bashrc && /bin/bash'
+    docker exec -it ros-dev bash -c '/bin/bash'
 else
     xhost +
-    docker-compose up & sleep 5 && docker exec -it ros-dev bash -c 'echo "source "/opt/ros/$ROS_DISTRO/setup.bash >> ~/.bashrc && /bin/bash'
+    docker-compose up & sleep 5 && docker exec -it ros-dev bash -c '/bin/bash'
     docker-compose down
     xhost -
 fi
